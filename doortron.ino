ec336bc61e0bbc696a81b1f5e42f2c27c1c37335
Anish Singhani <anish_oss@anishsinghani.com>
2023-01-16 21:22:25 -0500

    Create doortron.ino

---
+++ doortron.ino
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+#define M_URL0 "http://roboclubx.roboclub.org/doortron/_update.php?key=absolute_robolunkheads_1234&status=0"
+#define M_URL1 "http://roboclubx.roboclub.org/doortron/_update.php?key=absolute_robolunkheads_1234&status=1"
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println(WiFi.macAddress());
+  Serial.print("Connecting");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("CMU-DEVICE");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(D5, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // wait for WiFi connection
+  if ((WiFi.status() == WL_CONNECTED)) {
+
+    WiFiClient client;
+
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+
+    int q = digitalRead(D5);
+    Serial.println(q);
+    const char *murl = q ? M_URL0 : M_URL1;
+    
+    if (http.begin(client, murl)) {
+
+
+      Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.printf("[HTTP} Unable to connect\n");
+    }
+  }
+
+  delay(5000);
+}
