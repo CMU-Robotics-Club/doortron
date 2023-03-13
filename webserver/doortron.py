007004830f5fb4760a98b8bc3011e8f307e9da4a
Anish <anish@anishsinghani.com>
2023-03-13 00:32:40 -0400

    rejigger

---
+++ webserver/doortron.py
@@ -0,0 +1,22 @@
+import os
+import time
+from flask import Flask, render_template, request, redirect
+import threading
+
+state = (0, 0)
+
+app = Flask(__name__)
+
+with open("key.txt") as f:
+    key = f.read()
+
+with open("index.html") as f:
+    html = f.read()
+
+@app.route("/")
+def index():
+    return "good"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
