7a84a6843b725c4c8aeb9f14feca8001fb7648cb
root <root@baymax.lan.cmu.edu>
2025-09-16 15:56:14 -0400

    added :D

--- webserver/doortron.py
+++ webserver/doortron.py
@@ -16,7 +16,7 @@ COLORS = {STATE_CLOSED: "#8b0000", STATE_OPEN: "#006400", STATE_ERROR: "#444444"
 STATUS_TEXT1 = {STATE_CLOSED: "Roboclub is... ", STATE_OPEN: "Roboclub is... ", STATE_ERROR: ""}
 STATUS_TEXT2 = {STATE_CLOSED: "CLOSED", STATE_OPEN: "OPEN", STATE_ERROR: ""}
 INFO_TEXT = {STATE_CLOSED: "Contact an officer if you would like to get into the club.",
-             STATE_OPEN: "This means there's an officer at the club, feel free to drop by :)",
+             STATE_OPEN: "This means there's an officer at the club, feel free to drop by :D",
              STATE_ERROR: "The DoorTron system is currently offline, if this persists for more than 5 minutes please notify an officer"}
 
 SVGS = {}
@@ -38,12 +38,21 @@ def fmttime(x):
 def update0():
     global m_status
     m_status = (0, time.time())
+    with open ("doortron_log.csv", "r") as logfile:	
+        lines = logfile.readlines()
+        if (len(lines) == 0 or lines[-1].split(",")[0] == "closed"):
+            with open ("doortron_log.csv", "a") as logfile:
+                logfile.write("{},{}\n".format("open", fmttime(m_status[1])))
     return "OK"
-
 @app.route(f"/update/{THE_KEY}/1")
 def update1():
     global m_status
     m_status = (1, time.time())
+    with open ("doortron_log.csv", "r") as logfile:	
+        lines = logfile.readlines()
+        if (len(lines) == 0 or lines[-1].split(",")[0] == "open"):
+            with open ("doortron_log.csv", "a") as logfile:
+                logfile.write("{},{}\n".format("closed", fmttime(m_status[1])))
     return "OK"
 
 @app.route("/widget")
@@ -70,6 +79,7 @@ def api():
         state_name = "closed"
     elif state == STATE_ERROR:
         state_name = "unknown"
+    
     return json.dumps({
         "state": state_name
     })
@@ -79,7 +89,6 @@ def index():
     state = m_status[0]
     if time.time() - m_status[1] > 600:
         state = STATE_ERROR
-
     ts = ("(last updated " + fmttime(m_status[1]) + ")") if state in [STATE_OPEN, STATE_CLOSED] else ""
 
     return render_template("index.html", color=COLORS[state], status_text1=STATUS_TEXT1[state],
