301ac617d6fc8ee1e3cf9971f77e891f7c668b04
sky <sky@dragn.xyz>
2025-10-11 12:31:36 -0400

    change color back to green, gitignore heatmap

--- webserver/doortron.py
+++ webserver/doortron.py
@@ -7,15 +7,19 @@ from quart import Quart, render_template
 from quart_cors import cors, route_cors
 import asyncio
 import logging
-from systemd import journal
 
 from viridis import viridis
 
-# state stuff
-
+# logging
 log = logging.getLogger('doortron_py')
-log.addHandler(journal.JournaldLogHandler())
 log.setLevel(logging.INFO)
+try:
+    from systemd import journal
+    log.addHandler(journal.JournaldLogHandler())
+except Exception as _:
+    pass
+
+# state stuff
 
 with open("key.json") as f:
     keys = json.load(f)
@@ -45,10 +49,11 @@ try:
     with open("heatmap.npy", "rb") as f:
         heatmap_raw = np.load(f).astype("uint32")
     assert heatmap_raw.shape == (6, 7, 24, 2)
+    log.info("loaded persisted heatmap")
 except Exception as e:
-    print(f"failed to load heatmap and minutes: {e}")
-    print("creating new blank heatmap")
-    # 7 days * 24 hours array to track door open minutes
+    log.error(f"failed to load heatmap and minutes: {e}")
+    log.info("creating new blank heatmap")
+    # 6 weeks * 7 days * 24 hours * (open, total)
     heatmap_raw = np.zeros((6, 7, 24, 2), dtype="uint32")
 
 # tasks
@@ -69,10 +74,15 @@ async def task_roll_heatmap():
         sleep_seconds = (next_sunday - now).total_seconds()
         await asyncio.sleep(sleep_seconds)
 
-        # Run your task
+        # Roll heatmap forward
         heatmap_raw = heatmap_raw[1:, :, :, :]
         heatmap_raw = np.concatenate([heatmap_raw, np.zeros(1, 7, 24, 2)])
-        
+
+        log.info("rolled over heatmap")
+
+        # Note: even though the roll-over won't happen in the case that Doortron is turned off during
+        # Sunday midnight, that shouldn't be a problem (in that case one week's table will just contain
+        # 2 weeks' worth of data, but it will average out and eventually be cleared the same way)
 
 async def task_heatmap():
     """Runs once a minute: if door is open, increment the heatmap bucket."""
@@ -82,15 +92,17 @@ async def task_heatmap():
         now = datetime.now()
         day_idx = now.weekday()   # 0=Monday … 6=Sunday
         hour_idx = now.hour       # 0–23
+        # open minutes
         if club_door:
             heatmap_raw[-1, day_idx, hour_idx, 0] += 1
+        # total minutes
         heatmap_raw[-1, day_idx, hour_idx, 1] += 1
-        
+
         try:
             with open("heatmap.npy", "wb") as f:
                 np.save(f, heatmap_raw)
         except Exception as e:
-            print(f"failed to save heatmap: {e}")
+            log.error(f"failed to save heatmap: {e}")
 
 async def task_timeout():
     """Time out state if we haven't been updated in an hour"""
@@ -98,8 +110,10 @@ async def task_timeout():
     while True:
         await asyncio.sleep(60)
         if datetime.now() - club_last > timedelta(hours=1):
+            log.warning("club door timed out!")
             club_door = None
         if datetime.now() - shop_last > timedelta(hours=1):
+            log.warning("shop door timed out!")
             shop_door = None
 
 # webapp stuff
@@ -130,20 +144,23 @@ async def api():
 @app.route("/heatmap")
 @route_cors()
 async def get_heatmap():
-    """Expose the heatmap as JSON (7×24)"""
+    """Expose the full heatmap as JSON"""
     return heatmap_raw.tolist()
 
 @app.route("/")
 async def index():
     # compute heatmap
-    heatmap = np.sum(heatmap_raw, axis=0)
-    if np.max(heatmap[:, :, 1]) > 0:
-        heatmap = np.divide(heatmap[:, :, 0], heatmap[:, :, 1], where=heatmap[:, :, 1]!=0)
-        log.info(f"{heatmap.shape}")
-        heatmap = (heatmap * 255).astype("u1")
-        heatmap = viridis[heatmap]
-    else:
-        heatmap = np.full((7, 24), viridis[0])
+    all_weeks = np.sum(heatmap_raw, axis=0)
+    heatmap = np.zeros((7, 24)) # initialize to zeros to avoid uninit
+    np.divide(
+        all_weeks[:, :, 0], all_weeks[:, :, 1],
+        out=heatmap,
+        where=all_weeks[:, :, 1] != 0, # when false, use existing value (0)
+    )
+    heatmap = np.clip(heatmap, 0, 1)
+
+    heatmap = (heatmap * 255).astype("u1")
+    heatmap = viridis[heatmap]
 
     return await render_template(
         "index.html",
